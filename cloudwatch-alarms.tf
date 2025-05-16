4da2bc941f04e3fe49428282be1eac52bda34013
Cristian Beraha <cristian@cloudops.works>
2025-05-15 23:11:57 -0300

    Set empty actions for insufficient data in CloudWatch alarms
    Added `insufficient_data_actions` to explicitly define behavior when alarm data is insufficient. This ensures clarity and consistency in alarm configurations while maintaining existing behaviors.

--- cloudwatch-alarms.tf
+++ cloudwatch-alarms.tf
@@ -17,6 +17,7 @@ resource "aws_cloudwatch_metric_alarm" "cis_unauthorized_api_activity" {
   alarm_actions = [
     aws_sns_topic.cis_alarm_topic.arn,
   ]
-  treat_missing_data = "NotBreaching"
-  tags               = local.all_tags
+  treat_missing_data        = "NotBreaching"
+  insufficient_data_actions = []
+  tags                      = local.all_tags
 }
