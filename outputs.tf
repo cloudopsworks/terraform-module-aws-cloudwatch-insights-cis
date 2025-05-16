9ff723c594d354fb4ed9df033f78d6d844ea311c
Cristian Beraha <cristian@cloudops.works>
2025-05-16 10:11:32 -0300

    Add outputs for CIS SNS topic and unauthorized API alarm
    This commit introduces new outputs to expose the name and ARN of the CIS SNS topic, as well as the ARN for the unauthorized API activity alarm. These outputs improve module integration and accessibility for external references.

--- outputs.tf
+++ outputs.tf
@@ -4,3 +4,14 @@
 #            Distributed Under Apache v2.0 License
 #
 
+output "cis_sns_topic_name" {
+  value = aws_sns_topic.cis_alarm_topic.name
+}
+
+output "cis_sns_topic_arn" {
+  value = aws_sns_topic.cis_alarm_topic.arn
+}
+
+output "cis_unauthorized_api_activity_alarm_arn" {
+  value = aws_cloudwatch_metric_alarm.cis_unauthorized_api_activity.arn
+}
